ef1b4213117b86ab9a12a7e7cf4c003f954e2bf3
Gabriel Camargos <86752686+Gabrielcamargos28@users.noreply.github.com>
2023-09-19 15:21:09 -0300

    Subindo pro git

---
+++ src/main/java/com/gerenciamentodearquivos/Gerenciamento/GerenciamentoDeArquivosApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamentodearquivos.Gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeArquivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciamentodearquivos/Gerenciamento/GerenciamentoDeArquivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentodearquivos.Gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeArquivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
